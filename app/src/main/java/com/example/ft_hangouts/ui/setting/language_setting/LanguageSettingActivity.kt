f2bba3a18be8259b2763916452b8778720d658f1
seongjunkim <tjdwns5063@naver.com>
2023-04-21 15:21:07 +0900

    Implement so that the corresponding checkbox is activated according to the selected language.
    - Eliminate duplicate code.
    - Fix problem that cannot save language to sharedpreference when sdk version upper than 33.

--- app/src/main/java/com/example/ft_hangouts/ui/setting/language_setting/LanguageSettingActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/ui/setting/language_setting/LanguageSettingActivity.kt
@@ -18,20 +18,33 @@ class LanguageSettingActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setInitialLanguage()
+
         binding.languageBackButton.setOnClickListener { finish() }
         binding.languageEnCheckbox.setOnClickListener {
             cancelSelectionPrevLanguage(it)
             changeLanguage(it)
         }
+
         binding.languageDefaultCheckbox.setOnClickListener {
-            cancelSelectionPrevLanguage(it)
             cancelSelectionPrevLanguage(it)
             changeLanguage(it)
         }
         binding.languageKrCheckbox.setOnClickListener {
             cancelSelectionPrevLanguage(it)
-            cancelSelectionPrevLanguage(it)
             changeLanguage(it)
+        }
+    }
+
+    private fun setInitialLanguage() {
+        val language = sharedPreferenceUtils.getLanguage()
+
+        println("language : $language")
+
+        when(language) {
+            "ko" -> binding.languageKrCheckbox.isChecked = true
+            "en" -> binding.languageEnCheckbox.isChecked = true
+            else -> binding.languageDefaultCheckbox.isChecked = true
         }
     }
 
@@ -59,15 +72,14 @@ class LanguageSettingActivity : BaseActivity() {
 
     private fun changeLanguage(view: View) {
         val localeList = matchViewToLocaleList(view)
+        val localeString = if (localeList.isEmpty) null else localeList[0].language
 
+        sharedPreferenceUtils.setLanguage(localeString)
         if (Build.VERSION.SDK_INT >= 33) {
             val localManager = getSystemService(LocaleManager::class.java)
 
             localManager.applicationLocales = localeList
         } else {
-            val localeString = if (localeList.isEmpty) null else localeList[0].language
-            sharedPreferenceUtils.setLanguage(localeString)
-
             EventDialog.showEventDialog(
                 fragmentManager = supportFragmentManager,
             message = "언어를 변경하려면 앱을 재시작해야합니다. 재시작 하시겠습니까?",
