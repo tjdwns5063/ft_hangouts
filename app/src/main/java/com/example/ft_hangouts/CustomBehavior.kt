f4848dbfe852e9bba192c5be0c6b36602ba6e324
seongjunkim <tjdwns5063@naver.com>
2023-04-25 14:09:26 +0900

    Implementing a Behavior that reacts to the height of the Appbarlayout

--- app/src/main/java/com/example/ft_hangouts/CustomBehavior.kt
+++ app/src/main/java/com/example/ft_hangouts/CustomBehavior.kt
@@ -2,20 +2,17 @@ package com.example.ft_hangouts
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.max
 
 class CustomBehavior(context: Context, attrs: AttributeSet?) :
     CoordinatorLayout.Behavior<TextView?>(context, attrs) {
     private var startHeight = 0f
     private var startToolbarHeight = 0f
-
-    private val maxToolbarHeight = 150 * context.resources.displayMetrics.density
-    private val minToolbarHeight = 249f
-
+    private val finalToolbarHeight = 249f
     private var initialized = false
 
     override fun layoutDependsOn(
@@ -32,22 +29,17 @@ class CustomBehavior(context: Context, attrs: AttributeSet?) :
         dependency: View
     ): Boolean {
         if (dependency is AppBarLayout) {
-            Log.i("behavior", "changed called")
             initProperties(child, dependency)
 
-            val currentToolbarHeight =
-                startToolbarHeight + dependency.getY() // current expanded height of toolbar
+            val currentToolbarHeight = max(finalToolbarHeight - dependency.getY(), finalToolbarHeight)
+            val totalMoved = startToolbarHeight - finalToolbarHeight
+            val moved = startToolbarHeight - currentToolbarHeight
+            val progress = (moved / totalMoved * 100).toInt()
 
-            Log.i("behavior", "currHeight: $currentToolbarHeight")
-
-            //531, 249
-            val childAlpha = (currentToolbarHeight / minToolbarHeight) - 1
-            Log.i("behavior", "alpha: $childAlpha")
-            child.alpha = childAlpha
-
+            child.alpha = progress * 0.01f
             return true
         }
-        return super.onDependentViewChanged(parent!!, child, dependency)
+        return super.onDependentViewChanged(parent, child, dependency)
     }
 
     private fun initProperties(
@@ -55,10 +47,56 @@ class CustomBehavior(context: Context, attrs: AttributeSet?) :
         dependency: View
     ) {
         if (!initialized) {
-            // form initial layout
             startHeight = child.height.toFloat()
             startToolbarHeight = dependency.height.toFloat()
-            // some calculated fields
+            initialized = true
+        }
+    }
+}
+
+class ReverseCustomBehavior(private val context: Context, attrs: AttributeSet?) :
+    CoordinatorLayout.Behavior<TextView?>(context, attrs) {
+    private var startHeight = 0f
+    private var startToolbarHeight = 0f
+    private val finalToolbarHeight = 249f
+    private var initialized = false
+
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: TextView,
+        dependency: View
+    ): Boolean {
+        return dependency is AppBarLayout
+    }
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: TextView,
+        dependency: View
+    ): Boolean {
+        if (dependency is AppBarLayout) {
+            initProperties(child, dependency)
+            val currentToolbarHeight = max(finalToolbarHeight - dependency.getY(), finalToolbarHeight)
+            val totalMoved = startToolbarHeight - finalToolbarHeight
+            val moved = startToolbarHeight - currentToolbarHeight
+            val progress = moved / totalMoved
+
+            child.translationY = -(moved - (16 * context.resources.displayMetrics.density))
+            child.alpha = 1f - progress
+
+            return true
+        }
+        return super.onDependentViewChanged(parent, child, dependency)
+    }
+
+    private fun initProperties(
+        child: TextView,
+        dependency: View
+    ) {
+        if (!initialized) {
+            startHeight = child.height.toFloat()
+
+            startToolbarHeight = dependency.height.toFloat()
             initialized = true
         }
     }
