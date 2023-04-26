bbcfe118f52a00cac17a6d36e2905ea615531e01
seongjunkim <tjdwns5063@naver.com>
2023-04-26 18:36:51 +0900

    Modify from LiveData to StateFlow in ContactDetailViewModel.
    - changed it because StateFlow is easier to test than LiveData.

--- app/src/main/java/com/example/ft_hangouts/ui/detail/ContactDetailActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/ui/detail/ContactDetailActivity.kt
@@ -1,9 +1,13 @@
 package com.example.ft_hangouts.ui.detail
 
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.ft_hangouts.EventDialog
 import com.example.ft_hangouts.R
+import com.example.ft_hangouts.data.contact_database.ContactDatabaseDAO
+import com.example.ft_hangouts.data.contact_database.ContactHelper
 import com.example.ft_hangouts.databinding.ActivityContactDetailBinding
 import com.example.ft_hangouts.system.registerRequestCallPermissionResult
 import com.example.ft_hangouts.system.requestCallPermission
@@ -12,12 +16,15 @@ import com.example.ft_hangouts.ui.base.BaseActivity
 import com.example.ft_hangouts.ui.base.ContactActivityContract.CONTACT_ID
 import com.example.ft_hangouts.ui.edit.ContactEditActivity
 import com.example.ft_hangouts.ui.sms.ContactSmsActivity
+import kotlinx.coroutines.launch
 
 
 class ContactDetailActivity : BaseActivity() {
     private val binding: ActivityContactDetailBinding by lazy { ActivityContactDetailBinding.inflate(layoutInflater) }
     private val id by lazy { intent.getLongExtra(CONTACT_ID, -1) }
-    private val viewModel by lazy { ContactDetailViewModel(applicationContext, lifecycleScope, id, super.baseViewModel) }
+    private val viewModel by lazy {
+        ContactDetailViewModel(lifecycleScope, id, baseViewModel, ContactDatabaseDAO(ContactHelper(this)))
+    }
     private val callPermissionLauncher = registerRequestCallPermissionResult()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,14 +42,14 @@ class ContactDetailActivity : BaseActivity() {
     }
 
     private fun setContactObservationForProfileUpdates() {
-        viewModel.contact.observe(this) {
-            it.profile?.let { profile -> binding.detailProfileImage.setImageBitmap(profile) }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.updateContact()
+                viewModel.contact.collect {
+                    it.profile?.let { profile -> binding.detailProfileImage.setImageBitmap(profile) }
+                }
+            }
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.updateContact()
     }
 
     private fun setBottomNavItemListener() {
@@ -57,10 +64,7 @@ class ContactDetailActivity : BaseActivity() {
                 R.id.detail_bottom_sms -> { goToActivity(ContactSmsActivity::class.java, CONTACT_ID, id) }
                 R.id.detail_bottom_call -> {
                     requestCallPermission(callPermissionLauncher)
-                    viewModel.contact.value?.let {
-                        val phoneNumber = it.phoneNumber
-                        requestCallToCallSystemHelper(phoneNumber)
-                    }
+                    requestCallToCallSystemHelper(viewModel.contact.value.phoneNumber)
                 }
                 R.id.detail_bottom_edit -> { goToActivity(ContactEditActivity::class.java, CONTACT_ID, id) }
             }
--- app/src/main/java/com/example/ft_hangouts/ui/detail/ContactDetailViewModel.kt
+++ app/src/main/java/com/example/ft_hangouts/ui/detail/ContactDetailViewModel.kt
@@ -11,32 +11,30 @@ import com.example.ft_hangouts.error.*
 import com.example.ft_hangouts.ui.base.BaseViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ContactDetailViewModel(
-    context: Context,
     private val lifecycleScope: CoroutineScope,
     private val id: Long,
-    private val baseViewModel: BaseViewModel
+    private val baseViewModel: BaseViewModel,
+    private val contactDatabaseDAO: ContactDatabaseDAO
     ) {
-    private val contactDatabaseDAO = ContactDatabaseDAO(ContactHelper.createDatabase(context))
-
-    val contact: LiveData<ContactDomainModel>
-        get() = _contact
-    private val _contact = MutableLiveData<ContactDomainModel>()
+    private val _contact = MutableStateFlow<ContactDomainModel>(ContactDomainModel(-1, "", "", "", "", ""))
+    val contact: StateFlow<ContactDomainModel> = _contact.asStateFlow()
 
     init {
-        lifecycleScope.launch {
-            getContactById(id)
-        }
+        updateContact()
     }
 
     private suspend fun getContactById(id: Long) = withContext(Dispatchers.IO) {
         try {
             val contact = contactToContactDomainModel(contactDatabaseDAO.getItemById(id))
 
-            _contact.postValue(contact)
+            _contact.value = contact
         } catch (err: Exception) {
             baseViewModel.submitHandler(DatabaseReadErrorHandler())
         } finally {
@@ -54,10 +52,8 @@ class ContactDetailViewModel(
         }
     }
 
-    fun updateContact() {
-        lifecycleScope.launch {
-            getContactById(id)
-        }
+    fun updateContact() = lifecycleScope.launch {
+        getContactById(id)
     }
 
     fun deleteContact(id: Long) {
